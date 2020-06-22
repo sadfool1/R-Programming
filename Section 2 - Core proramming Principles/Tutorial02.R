b725829615bb96b535dddc0644ff207fc9971e21
James <takw14@gmail.com>
2020-06-22 18:44:41 +0800

    Initial commit

---
+++ Section 2 - Core proramming Principles/Tutorial02.R
@@ -0,0 +1,23 @@
+A <- 10
+B <- 5
+C <- A + B
+C
+
+#variable1
+var1 <- 2.5
+#variable2
+var2 <- 4
+
+#division
+result <- var1 / var2
+result
+
+#sqrt
+answer <- sqrt (var2)
+answer
+
+#string concatenation
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+message
