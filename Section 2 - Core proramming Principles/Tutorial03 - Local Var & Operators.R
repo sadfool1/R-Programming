bbe185b07fb54fa297198bdfbdb2d6be158fa01a
James <takw14@gmail.com>
2020-06-22 19:10:27 +0800

    File Management

---
+++ Section 2 - Core proramming Principles/Tutorial03 - Local Var & Operators.R
@@ -0,0 +1,32 @@
+#logical:
+#TRUE T
+#FALSE F
+
+4 < 5
+10 > 100
+4 == 5 #False
+
+# == 
+# != 
+# >
+# <
+# <= 
+# >=
+# !
+result <- 4 < 5
+result
+typeof(result)
+
+# | (works like an "or")
+result2 <- !(5>1)
+result | result2 #TRUE
+result & result2 #FALSE
+
+# &
+# isTRUE(x)
+
+isTRUE (result) #TRUE since result is TRUE
+
+
+
+
