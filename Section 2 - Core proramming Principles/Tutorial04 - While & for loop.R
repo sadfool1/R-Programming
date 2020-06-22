ef8aeaecabe8e320ae08ccf2369f83bbb7a948d5
James <takw14@gmail.com>
2020-06-22 19:23:12 +0800

    While & for loop

---
+++ Section 2 - Core proramming Principles/Tutorial04 - While & for loop.R
@@ -0,0 +1,20 @@
+
+#infinite loop 
+while(FALSE){
+  print ("Hello")
+}
+
+#while loop with counter
+counter <- 1
+while(counter < 12){
+  
+  print (counter)
+  cou
+  nter <- counter +1 
+}
+#for loop
+for(i in 1:5){ #iteration from 1 to 5
+	print ("Hello R")
+}
+
+#1:5 is a sequence from 1 to 5
